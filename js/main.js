cf2e6b01d8a42fca430424c3a26ac45643aecdf0
marcin <brand.marcin@gmail.com>
2023-01-31 17:22:23 +0000

    zadanie43654

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-let bday = '31'
-let bmonth = '5'
-let byear = '1990'
+let bday = '31';
+let bmonth = '5';
+let byear = '1990';
 
-let Array = [31, 5, 1990]
+let array = [31, 5, 1990]
 
-console.log(Array)
+console.log(array)
